97ba689bfec037a118a9e63472c9fc3326e3ccde
Yagnes Salomão <yrsalomao@gmail.com>
2020-09-23 18:16:01 -0300

    Finished animations

--- js/script.js
+++ js/script.js
@@ -24,4 +24,27 @@ $(document).ready(function() {
 
 
     /* Animations on Scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset:'50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    }, {
+        offset:'50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
+        offset:'50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    }, {
+        offset:'50%'
+    });
 });
