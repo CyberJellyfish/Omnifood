6523b89d1b2820352c774420f1c06bfb9910a1b7
Yagnes Salomão <yrsalomao@gmail.com>
2020-09-23 17:08:20 -0300

    Scroll to links

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
 
+    /*For the sticky navigation */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky');
         }
-    }) 
+    }, {
+        offset: '60px;'
+    });
+
+
+
+    /*Scroll on buttons*/
+    $('.js--scroll-to-plans').on("click", function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').on("click", function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+    /* Animations on Scroll */
 });
